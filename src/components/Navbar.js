6bb3f8e208553842e5a42c284f0a8c82320b20d8
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-11-08 16:21:58 +0200

    Create navbar with links to different pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import planet from '../images/planet.png';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Rockets',
+      name: 'rockets',
+    },
+    {
+      id: 2,
+      path: '/missions',
+      text: 'Missions',
+      name: 'missions',
+    },
+    {
+      id: 3,
+      path: '/my-profile',
+      text: 'My Profile',
+      name: 'my-profile',
+    },
+  ];
+
+  return (
+    <nav className="navbar">
+      <header className="logo-container">
+        <img src={planet} alt="Logo" className="logo" />
+        <h1 className="title">Space Travelers&rsquo; Hub</h1>
+      </header>
+      <ul className="links">
+        {links.map((link) => (
+          <li key={link.id} className={link.name}>
+            <NavLink to={link.path}>
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
