1d3dd466fd2bf100a58d0e5955c8dcf9f035d588
wuyepabdul <wuyepabdul@gmail.com>
2021-11-08 23:16:24 +0100

    Fetch data from api

--- src/Pages/Rockets.js
+++ src/Pages/Rockets.js
@@ -1,5 +1,33 @@
-const Rockets = () => (
-  <h1>Rockets coming soon...</h1>
-);
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRocketsAction } from '../store/rockets/rockets';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const { rockets } = useSelector((state) => state.rockets);
+
+  const loadRockets = () => {
+    dispatch(getRocketsAction());
+  };
+  useEffect(() => {
+    loadRockets();
+  }, []);
+
+  return (
+    <div>
+      {rockets && rockets.map((rocket) => (
+        <li key={rocket.id}>
+          {rocket.description}
+          {' '}
+          <br />
+          <br />
+          <br />
+        </li>
+
+      ))}
+      {/* {rockets ? console.log('rockets', rockets) : console.log('no rockets', rockets)} */}
+    </div>
+  );
+};
 
 export default Rockets;
--- src/store/rockets/rockets.js
+++ src/store/rockets/rockets.js
@@ -4,14 +4,14 @@ const GET_ROCKETS = 'space-trevelers-hub/rockets/GET_ROCKETS';
 const SET_ROCKETS = 'space-travelers-hub/rockets/SET_ROCKETS';
 const ERROR_ROCKETS = 'space-travelers-hub/rockets/ERROR_ROCKETS';
 
-const initialState = [];
+const initialState = { rockets: null };
 
 export const getRocketsAction = () => async (dispatch) => {
   const baseUrl = 'https://api.spacexdata.com/v3/rockets';
   try {
     dispatch({ type: GET_ROCKETS });
-    const rockets = await axios.get(`${baseUrl}`);
-    dispatch({ type: SET_ROCKETS, payload: rockets });
+    const { data } = await axios.get(`${baseUrl}`);
+    dispatch({ type: SET_ROCKETS, payload: data });
   } catch (error) {
     dispatch({ type: ERROR_ROCKETS, payload: error.message });
   }
@@ -22,7 +22,7 @@ export default function rocketsReducer(state = initialState, action) {
     case GET_ROCKETS:
       return state;
     case SET_ROCKETS:
-      return [...state, action.payload];
+      return { ...state, rockets: action.payload };
     default:
       return state;
   }
