212ef27c0db05b97825cb5eabb1cf2039cac7df1
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-11-08 16:22:17 +0200

    Add pages and navbar to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Missions from './Pages/Missions';
+import MyProfile from './Pages/MyProfile';
+import Rockets from './Pages/Rockets';
 
-const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
-}
+const App = () => (
+  <Router>
+    <Navbar />
+    <Switch>
+      <Route exact path="/">
+        <Rockets />
+      </Route>
+      <Route path="/missions">
+        <Missions />
+      </Route>
+      <Route path="/my-profile">
+        <MyProfile />
+      </Route>
+    </Switch>
+  </Router>
+);
 
-export default App
+export default App;
