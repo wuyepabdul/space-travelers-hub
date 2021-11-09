e3863e8ba5329e784a2d8763a2ca1414993f328f
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-11-09 15:00:25 +0200

    Add mission reducer to store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,9 +3,11 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rocketsReducer from './rockets/rockets';
+import { MissionReducer } from './missions/missionsReducer';
 
 const reducers = combineReducers({
   rockets: rocketsReducer,
+  MissionReducer,
 });
 
 const middlwares = [thunk, logger];
