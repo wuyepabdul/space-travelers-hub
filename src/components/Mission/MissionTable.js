efbda79eef1ad10629f708004b8cab2f04d9cc84
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-11-09 15:00:44 +0200

    Populate table with data

--- src/components/Mission/MissionTable.js
+++ src/components/Mission/MissionTable.js
@@ -1,9 +1,21 @@
-import { missionDetails, columns } from './MissionsDetails';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import FetchMission from '../../store/missions/missionsAPI';
+import { GetMissions } from '../../store/missions/missionsReducer';
 
 const MissionTable = () => {
-  const details = missionDetails.map((mission) => (
-    <tr key={mission.id}>
-      <td className="missionName">{mission.mission}</td>
+  const MissionStore = useSelector((store) => store.MissionReducer);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    FetchMission()
+      .then((response) => dispatch(GetMissions(response)));
+  }, []);
+
+  const details = MissionStore.map((mission) => (
+    <tr key={mission.mission_id}>
+      <td className="missionName">{mission.mission_name}</td>
       <td className="missionDescription">{mission.description}</td>
       <td className="missionStatus">
         <button className="statusButton" type="button">NOT A MEMBER</button>
@@ -14,16 +26,43 @@ const MissionTable = () => {
     </tr>
   ));
 
+  const columns = [
+    {
+      Header: 'Mission',
+      accessor: 'mission',
+      name: 'mission-title',
+    },
+    {
+      Header: 'Description',
+      accessor: 'description',
+      name: 'mission-description',
+    },
+    {
+      Header: 'Status',
+      accessor: 'status',
+      name: 'mission-status',
+    },
+    {
+      Header: ' ',
+      accessor: 'join',
+      name: 'mission-join',
+    },
+  ];
+
   return (
     <table className="mission-table">
-      <tr>
-        {columns.map((column) => (
-          <th key={column.accessor} className={column.name}>
-            {column.Header}
-          </th>
-        ))}
-      </tr>
-      {details}
+      <tbody>
+        <tr>
+          {columns.map((column) => (
+            <th key={column.accessor} className={column.name}>
+              {column.Header}
+            </th>
+          ))}
+        </tr>
+      </tbody>
+      <tbody>
+        {details}
+      </tbody>
     </table>
   );
 };
